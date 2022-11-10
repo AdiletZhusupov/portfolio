c7eb502ea64d61e68a9e63a91c09f8aec066a70d
AdiletZhusupov <adilet.zhusupov@gmail.com>
2022-11-10 10:13:10 +0300

    back to scroll

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
